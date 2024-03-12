b91567b1fca0172d4b0763aad6624cfe9f8b0365
AHMED BILAL <162837323+Ahmed-Bilal-dev@users.noreply.github.com>
2024-03-12 23:43:07 +0500

    chap-1 (alert) assignment

---
+++ chapter-1/app.js
@@ -0,0 +1,21 @@
+// Builtin Functions
+alert("Hello Ahmed Bilal.");
+alert("Error!please enter a valid password.");
+alert("Welcome to js Land... \n Happy coding.");
+alert("Welcome to js Land");
+alert("Happy coding.");
+
+
+// console... show on inspect console
+console.log("Hello...I can run JS through my web browser's console");
+console.log("Error!please enter a valid password.");
+console.log("Welcome to js Land... \n Happy coding.");
+console.log("Welcome to js Land");
+console.log("Happy coding.");
+
+// document... show on browser
+document.write("Hello...My name is Ahmed Bilal<br/>");
+document.write("Error!please enter a valid password.<br/>");
+document.write("Welcome to js Land... \n Happy coding.<br/>");
+document.write("Welcome to js Land<br/>")
+document.write("Happy coding.<br/>");
