0ce78bf823df37d1b72bc4fe12adae4a5a950e31
AHMED BILAL <162837323+Ahmed-Bilal-dev@users.noreply.github.com>
2024-05-19 12:12:30 -0700

    chapter 21 to 25

---
+++ chapter-21 to 25/app.js
@@ -0,0 +1,53 @@
+// chapter 21 to 25
+
+// QNO1.Write a program that takes two user inputs for first and
+// last name using prompt and merge them in a new variable
+// titled fullName. Greet the user using his full name. 
+
+// ANSWER OF QNO1
+// let userInput1 = prompt("Enter your First Name");
+// let userInput2 = prompt("Enter your Last Name");
+// document.write("hello " + userInput1 + " " + userInput2);
+
+// ------------------------------------------------------------------------------------------------
+
+// QNO2.Write a program to take a user input about his favorite
+// mobile phone model. Find and display the length of user
+// input in your browser
+
+// ANSWER OF QNO2
+// let userInput = prompt("Enter your favourite phone name");
+// document.write(userInput+ '<br/>');
+// let userLength = userInput.length;
+// document.write("Length of string are" + ' ' + userLength);
+
+// ------------------------------------------------------------------------------------------------
+
+// QNO3.Write a program to find the index of letter “n” in the word
+// “Pakistani” and display the result in your browser .
+
+// ANSWER OF QNO3
+// let pakistaniSpelling = ("pakistani");
+// document.write("string is " + pakistaniSpelling + '<br/>');
+// document.write("index of 'n' is " +  pakistaniSpelling.indexOf("n"));
+
+
+// ------------------------------------------------------------------------------------------------
+
+// QNO4.Write a program to find the last index of letter “l” in the
+// word “Hello World” and display the result in your browser.
+
+// ANSWER OF QNO4
+// let string = ("Hello World");
+// document.write("string is " + string + '<br/>');
+// document.write("last index of 'l' is " +  string.lastIndexOf("l"));
+
+//------------------------------------------------------------------------------------------------
+
+// QNO5.Write a program to find the character at 3rd index in the
+// word “Pakistani” and display the result in your browser.
+
+// ANSWER OF QNO5
+let pakistaniSpelling = ("pakistani");
+document.write("string is " + pakistaniSpelling + '<br/>');
+document.write("Character at index 3 is: " +  pakistaniSpelling.charAt("3"));
